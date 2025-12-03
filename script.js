4409f1793624b3490601a6eb4932e1feb2ea5530
meghamanju210-crypto <meghamanju210@gmail.com>
2025-12-03 12:47:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openSearch() {
+    document.getElementById("searchBar").style.display = "flex";
+}
+
+function closeSearch() {
+    document.getElementById("searchBar").style.display = "none";
+}
